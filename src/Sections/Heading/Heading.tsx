715ff3d9f3c96554fa117d8ada9f375dd40a391f
Dola Barai <dola43dc@gmail.com>
2023-08-26 13:15:02 +0600

    heading added

---
+++ src/Sections/Heading/Heading.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const Heading = () => {
+
+    return (
+        <div>
+            <div className="navbar -py-4 bg-[#322653] px-8">
+                <div className="navbar-start">
+                    <div className="flex gap-3 ">
+                        <h1 className="normal-case text-xs text-white hover:text-[#9288F8]">About us</h1>
+                        <h1 className="normal-case text-xs text-white">|</h1>
+                        <h1 className="normal-case text-xs text-white hover:text-[#9288F8]">Careers</h1>
+                        <h1 className="normal-case text-xs text-white">|</h1>
+                        <h1 className="normal-case text-xs text-white hover:text-[#9288F8]">Open a Shop</h1>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <h3 className="normal-case text-sm text-white">Enjoy Free Shipping on Orders Over <span className="text-green-400 text-sm">$75.00</span></h3>
+                </div>
+                <div className="navbar-end">
+                    <h3 className="normal-case text-xs text-white me-3">Need help? Call Us: <span className="text-green-400 text-sm">+8801712-345678</span></h3>
+                    <select className="normal-case text-xs text-white bg-transparent">
+                        <option className="bg-black" selected value="a">English</option>
+                        <option className="bg-black" value="Bangla">Bangla</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Heading;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Banner from "@/components/Banner/Banner"
-import Delivery from "@/components/Delivery/Delivery"
-import Navbar from "@/components/Navbar/Navbar"
+import Heading from "@/Sections/Heading/Heading";
 
 export default function Home() {
   return (
     <main className="text-3xl font-bold">
-           <Navbar></Navbar>
-           <Banner></Banner>
-           <Delivery></Delivery>
-            <p>Hello Next.js</p>
+         <Heading></Heading>
     </main>
   )
 }
